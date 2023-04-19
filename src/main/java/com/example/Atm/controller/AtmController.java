c63d6c6be88d7fb97b250b1691c0e14c03f29158
Ehsan450 <ehsanananno32@gmail.com>
2023-04-19 16:06:10 +0600

    Response Entity added

--- src/main/java/com/example/Atm/controller/AtmController.java
+++ src/main/java/com/example/Atm/controller/AtmController.java
@@ -1,5 +1,17 @@
 package com.example.Atm.controller;
 
+import com.example.Atm.entity.Card;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class AtmController {
+    @GetMapping("/deposit")
+    public ResponseEntity<?> depositBalance(@ModelAttribute Card card) {
+        // atmService.deposit(actions.getCardNo(), actions.getPin(), actions.getAmount());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT,HttpStatus.OK);
+    }
 
 }
